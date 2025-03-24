d6e19f5a7526b889e409c85d35bdd1ec166dbf16
alex <alex@gmail.com>
2025-03-24 14:32:30 +0200

    added tamplates to the script and modifed README file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-### Welcome to my script templates
+# Script and Project Tampltes
+
+## Here youll find my private script and project tamplates that will help you start your scripts with ease!
+
+Hey dear users, here you will find different tamplates that i use in my day to day work, homework, studies ect...
+The idea of this repo is to ease our work and to have ready and working scripts on stand by.
+Please do take in mind this is still work in progress and may have ocational issues, slowly but surly they will get patched and updated.
+
+
+## Contents
+
+- Safe Header: A safe header that i use to start every single script i write so it will help me to undrstand nad remember what is the script about and when it was created.
+- workstation-config: script that uses a source text file containing all the neccary packages to be downloaded on a new VM or workstation.
+
+
+
+
+
+
+## Contributors
+
+- You can find it here [Link](CONTRIBUTORS.md)
+
---
+++ workstation-config.sh
@@ -0,0 +1,56 @@
+#!/etc/bin/env bash
+##################################### Start Safe Header ############################
+#Developed by Alex Umansky aka TheBlueDrara
+#Porpuse: Deploy A virtual environment to work with pip and python package dependencies
+#Date 23.03.2025
+set -o nounset
+set -o errexit
+set -o pipefail
+##################################### End Safe Header ###############################
+
+
+# HERE YOU WILL FIND ALL THE DIFFERENT TAMPLATES
+
+####################################################################################
+************************************************************************************
+************************************************************************************
+############ Checks if the OS is debian based ######################################
+
+source /etc/os-realse
+
+if [[ $ID_LIKE == "*debian*" ]]; then
+	echo "Running on Debian-family distro. Executing main code..."
+else
+	echo "This script is designed to run only on Debian-family distory only!"
+	exit 1
+fi
+
+####################################################################################
+************************************************************************************
+************************************************************************************
+############# A simple for loop to check and install dependencies ##################
+
+source tool_list.txt # Create a text file with a var named TOOLS that contains a list of tools you want to download
+
+function main(){
+	for index in TOOLS; do
+		check_and_install "$index"
+	done
+}
+
+function check_and_install(){
+	package=$1
+	if ! dpkg -l | grep -q "^ii $package "; then
+		sudo apt-get install "$package" -y
+		return 1
+	else
+		return 0
+	fi
+}
+
+main
+
+#####################################################################################
+*************************************************************************************
+*************************************************************************************
+#####################################################################################
